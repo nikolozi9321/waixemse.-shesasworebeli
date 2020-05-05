dec40ef1d5d5b7ccb51c5f3a7c94d75abd97dde6
nika <61551590+nikolozi9321@users.noreply.github.com>
2020-05-06 01:28:26 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// "use strict";
+
+// let buttons = document.getElementsByClassName("button");
+
+// let arrayLength = buttons.length;
+
+
+// let styles = {
+//   backgroundColor: "#ECF0F1",
+//   color: "#BDC3C7",
+//   border: "none",
+// };
+
+// for (let i = 0; i < arrayLength; i++) {
+//   buttons[i].addEventListener("click", function () {
+//     if (this.textContent == "დამატება") {
+//       this.textContent = "დამატებულია";
+    
+
+//       for (let prop in styles) {
+    
+//         this.style[prop] = styles[prop];
+//       }
+//     } else {
+//       this.textContent = "დამატება";
+    
+//       for (let prop in styles) {
+//         this.style.removeProperty(prop);
+//       }
+//       this.style.removeProperty("background-color");
+//     }
+//   });
+// }
