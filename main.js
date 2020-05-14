c9e7eac7f3386642a4d1bf61468263c0f31414e4
nika <61551590+nikolozi9321@users.noreply.github.com>
2020-05-14 16:31:47 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -17,47 +17,45 @@ for(let i = 0; i < bl; i++){
 }
 
 
-// open overlay
 document.addEventListener("click", function () {
 	document.getElementById("burger-menu").onclick = openOverlay;
-	// close overlay on another click
+
+
 	if (document.querySelector(".overlay").style.width == "100%") {
 	  document.getElementById("burger-menu").onclick = closeOverlay;
 	}
   });
   
-  // Open Overlay func
+
   function openOverlay() {
-	// set width to 100% to show up overlay
+
 	document.querySelector(".overlay").style.width = "100%";
-	// disable scrolling
+
 	document.querySelector("body").style.overflow = "hidden";
-	// change image on click
+	
 	document.getElementById("burger-icon").style.display = "none";
 	document.getElementById("close-burger-icon").style.display = "block";
   }
   
-  // Close Overlay func
   function closeOverlay() {
-	// set width to 0px to hide overlay
+
 	document.querySelector(".overlay").style.width = "0px";
-	// enable scrolling
+	
 	document.querySelector("body").style.overflow = "visible";
-	// change image on click
+	
 	document.getElementById("close-burger-icon").style.display = "none";
 	document.getElementById("burger-icon").style.display = "block";
   }
   
-  // overlay will be closed, when you click nav links in menu
-  // get all overlay nav links
-  let list = document.getElementsByClassName("overlayNavLinks");
+
+//   let list = document.getElementsByClassName("overlayNavLinks");
   
-  let lengthOfList = list.length;
-  // event handler
-  for (let i = 0; i < lengthOfList; i++) {
-	list[i].addEventListener("click", function () {
-	  closeOverlay();
-	});
-  }
+//   let lengthOfList = list.length;
+
+//   for (let i = 0; i < lengthOfList; i++) {
+// 	list[i].addEventListener("click", function () {
+// 	  closeOverlay();
+// 	});
+//   }
 
 
